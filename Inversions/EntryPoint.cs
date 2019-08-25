ec0139f582e42038c43746d1b02988b52bfa23ca
lorenzofmann <lorenzofman@gmail.com>
2019-08-25 14:37:27 -0300

    Add inversions brute force and optimized algorithms

---
+++ Inversions/EntryPoint.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Inversions
+{
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            Inversions inversions = new Inversions();
+            List<int> randomList = RandomList(elements: 800, maxValue: 100);
+            PrintList(randomList);
+            inversions.BruteForceInversions(randomList);
+            inversions.FastInversions(randomList);
+        }
+
+        static List<int> RandomList(int elements, int maxValue)
+        {
+            List<int> list = new List<int>(elements);
+            Random random = new Random();
+            for(int i = 0; i < elements; i++)
+            {
+                list.Add(random.Next() % maxValue);
+            }
+            return list;
+        }
+
+        static void PrintList<T>(IReadOnlyList<T> list)
+        {
+            for(int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine($"{i} - {list[i]}");
+            }
+        }
+    }
+}
---
+++ Inversions/Inversions.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inversions
+{
+    public class Inversions
+    {
+        public void BruteForceInversions(List<int> list)
+        {
+            int inversions = 0;
+            Console.WriteLine("# Brute Force Inversions #");
+            for (int i = 0; i < list.Count; i++)
+            {
+                for(int j = i + 1; j < list.Count; j++)
+                {
+                    if(list[i] > list[j])
+                    {
+                        inversions++;
+                    }
+                }
+            }
+            Console.WriteLine($"Inversions: {inversions}");
+        }
+
+        public void FastInversions(List<int> list)
+        {
+            Console.WriteLine("# O(n*log n) Inversions #");
+            FastInv(list.ToArray());
+        }
+
+
+
+        private void FastInv(int[] array)
+        {
+            int inversions = FastInv(array, 0, array.Length - 1);
+            Console.WriteLine($"Inversions: {inversions}");
+        }
+
+        protected int FastInv(int[] array, int left, int right)
+        {
+            if (left == right)
+                return 0;
+            int middle = (left + right) / 2;
+            int inversions = 0;
+            inversions += FastInv(array, left, middle);
+            inversions += FastInv(array, middle + 1, right);
+            return inversions + Merge(array, left, middle, right);
+        }
+
+        protected int Merge(int[] array, int left, int middle, int right)
+        {
+            int inversions = 0;
+            int[] leftArray = new int[middle - left + 1];
+            int[] rightArray = new int[right - middle];
+
+            for (int i = 0; i < leftArray.Length; i++)
+            {
+                leftArray[i] = array[left + i];
+            }
+
+            for (int j = 0; j < rightArray.Length; j++)
+            {
+                rightArray[j] = array[middle + j + 1];
+            }
+
+            int leftIndex = 0, rightIndex = 0;
+
+            int k = left;
+
+            while (leftIndex < leftArray.Length && rightIndex < rightArray.Length)
+            {
+                if (leftArray[leftIndex].CompareTo(rightArray[rightIndex]) <= 0)
+                {
+                    array[k++] = leftArray[leftIndex++];
+                }
+                else
+                {
+                    inversions += leftArray.Length - leftIndex;
+                    array[k++] = rightArray[rightIndex++];
+                }
+            }
+
+            while (leftIndex < leftArray.Length)
+            {
+                array[k++] = leftArray[leftIndex++];
+            }
+
+            while (rightIndex < rightArray.Length)
+            {
+                array[k++] = rightArray[rightIndex++];
+            }
+            return inversions;
+        }
+    }
+}
