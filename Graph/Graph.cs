d4de23e7e19bfe9af33fe0c6ea84a80d753ec3e4
Lorenzofman <lorenzofman@gmail.com>
2019-10-23 11:31:53 -0300

    Add graph classes with DFS implementation

---
+++ Graph/Graph.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Graph
+{
+    public class Graph<T> where T: IComparable
+    {
+        public Node<T> firstNode;
+
+        public Graph(Node<T> node)
+        {
+            this.firstNode = node;
+        }
+
+        public Node<T> DepthFirstSearch(T element, bool verbose = false )
+        {
+            return DepthFirstSearch(firstNode, element, new HashSet<Node<T>>(), verbose);
+        }
+
+        private Node<T> DepthFirstSearch(Node<T> currentNode, T element, HashSet<Node<T>> visitedNodes, bool verbose)
+        {
+            Console.WriteLine($"Visiting node: {currentNode}");
+            if (visitedNodes.Contains(currentNode))
+            {
+                if (verbose)
+                {
+                    Console.WriteLine("\tReturning because node was already visited");
+                }
+                return null;
+            }
+
+            if(currentNode.data.CompareTo(element) == 0)
+            {
+                if(verbose)
+                {
+                    Console.WriteLine($"\tElement found\n");
+                }
+                return currentNode;
+            }
+
+            if (verbose)
+            {
+                Console.WriteLine($"\tElement not found, adding node to visitedNodes\n");
+            }
+            visitedNodes.Add(currentNode);
+
+            int i = 0;
+            foreach(Node<T> neighboor in currentNode.neighboors)
+            {
+                Console.WriteLine($"Entering {currentNode} neighboor: {i++}");
+                Node<T> neighboorSearch = DepthFirstSearch(neighboor, element, visitedNodes, verbose);
+                if (neighboorSearch != null)
+                {
+                    return neighboorSearch;
+                }
+            }
+
+            return null;
+        }
+    }
+
+}
---
+++ Graph/Node.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Graph
+{
+    public class Node<T> where T : IComparable
+    {
+        public T data;
+        public List<Node<T>> neighboors = new List<Node<T>>();
+
+        public Node(T data)
+        {
+            this.data = data;
+        }
+
+        public void AddNeighboor(params T[] neighboors)
+        {
+            foreach (T neighboor in neighboors)
+            {
+                Node<T> neighboorNode = new Node<T>(neighboor);
+                AddNeighboor(neighboorNode);
+            }
+        }
+
+        public void AddNeighboor(params Node<T>[] neighboorNodes)
+        {
+            foreach (Node<T> neighboorNode in neighboorNodes)
+            {
+                this.neighboors.Add(neighboorNode);
+                neighboorNode.neighboors.Add(this);
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Node<{ data.ToString() }>";
+        }
+    }
+}
